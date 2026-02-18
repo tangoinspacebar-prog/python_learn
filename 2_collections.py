55b3e63636d0a5292d7fc7ccd6f63258824625e6
tangoinspacebar-prog <tangoinspacebar@gmail.com>
2026-02-18 14:07:08 +0100

    Update 2_collections.py

--- 2_collections.py
+++ 2_collections.py
@@ -1,4 +1,3 @@
-# version 1
 # importing library to support generation of random numbers
 import random
 import string
@@ -8,47 +7,37 @@ import string
 # dict's values should be a number (0-100)
 
 # define list and randomized list length with counter
-list = []
-list_c = 1
-list_len_r = random.randint(2, 10)
-print(list_len_r)
-# define for dict a randomized dict length with counter
-# dict = {}
-dict_c = 1
-dict_len_r = random.randint(2, 10)
-print(dict_len_r)
+gen_list = []
+gen_list_len = random.randint(2, 10)
+print(gen_list_len)
 
 # define loop for list
-while list_c <= list_len_r:
+while len(gen_list) < gen_list_len:
     # prepare dict for processing in a loop
     # list stores pointers to dicts, so i cannot clear same dict for reusage, i need to recreate it (with same name)
-    dict = {}
-    dict_c = 1
+    gen_dict = {}
+    gen_dict_len = random.randint(2, 10)
     # randomize dict using random dict length, random keys, random values
     # do assignment in the loop, as length is random
-    while dict_c <= dict_len_r:
-        dict.update({random.choice(string.ascii_lowercase): random.randrange(100)})
-        dict_c = dict_c + 1
-    # print('dict ' + str(list_c) + ': ' +str(dict))
+    while len(gen_dict) < gen_dict_len:
+        gen_dict.update({random.choice(string.ascii_lowercase): random.randint(0,100)})
     # assign result to list
-    list.append(dict)
-    list_c = list_c+1
-    # print(list)
-print(list)
+    gen_list.append(gen_dict)
+print(gen_list)
 
 
 # 2. get previously generated list of dicts and create one common dict:
 # prepare variables for processing
 summary_dict = {}
-tmp_key = '~'
-tmp_value = 0
+#tmp_key = '~'
+#tmp_value = 0
 tmp_skip = False
 tmp_modify = False
 
 # take each dict in the list
-for i in range(list_len_r):
+for i in range(len(gen_list)):
     # for each item in the dict
-    for key, value in list[i].items():
+    for key, value in gen_list[i].items():
 
         # if already found, skip
         for k, v in summary_dict.items():
@@ -62,10 +51,10 @@ for i in range(list_len_r):
             tmp_value = value
 
             # found in cycle max value + define if need to modify key
-            for j in range(i, list_len_r):
-                if key in list[j].keys():
-                    if list[j][key] > tmp_value:
-                        tmp_value = list[j][key]
+            for j in range(i, len(gen_list)):
+                if key in gen_list[j].keys():
+                    if gen_list[j][key] > tmp_value:
+                        tmp_value = gen_list[j][key]
                         if j!=i:
                             tmp_key = key+'_'+str(j+1)
                     if j!=i:
